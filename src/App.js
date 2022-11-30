28143e598133eea588133531044bd9ca716e0edf
Priyosmita <priyosh08@gmail.com>
2022-11-30 17:22:25 +0530

    nav bar done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-
-import './App.css';
+import "./App.css";
+import Header from "./Header";
+import "./Header.css";
+import Home from "./Home";
 
 function App() {
   return (
     <div className="app">
-      <h1>Hello clever programmers :rocket
-
-      </h1>
+      {/* Header */}
+      <Header />
+      {/* Home */}
+      <Home />
     </div>
   );
 }
