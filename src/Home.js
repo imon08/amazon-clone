2a95f22bc3cccaa22e87f5febae14ca2aa6b120f
Priyosmita <priyosh08@gmail.com>
2022-11-30 22:58:09 +0530

    home page start

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,30 @@
-import React from 'react'
-import "./Home.css"
+import React from "react";
+import "./Home.css";
+import Product from "./Product";
 
 function Home() {
   return (
-    <div className='home'>
-        <div className="home__container">
-            
-        </div>
+    <div className="home">
+      <div className="home__container">
+        <img
+          className="home__image"
+          src="https://wallpaperaccess.com/full/6385972.jpg"
+          alt=""
+        />
+      </div>
+      <div className="home__row">
+        {/*Product*/}
+        <Product />
+        {/*Product*/}
+      </div>
+      <div className="home__row">
+        {/*Product*/}
+        {/*Product*/}
+        {/*Product*/}
+      </div>
+      <div className="home__row">{/*Product*/}</div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
