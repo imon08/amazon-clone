877ad9112daba0226a2b22cfb62b62cebb3b0c4a
Priyosmita <priyosh08@gmail.com>
2022-11-30 13:54:05 +0530

    initialize amazon-clone

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCKSjsmX-9Buo21x_--bVJ5_jBxr9hptrY",
+    authDomain: "challenge-8b5c0.firebaseapp.com",
+    projectId: "challenge-8b5c0",
+    storageBucket: "challenge-8b5c0.appspot.com",
+    messagingSenderId: "133271271323",
+    appId: "1:133271271323:web:c2ff8ae615b0d370f5533e",
+    measurementId: "G-063J33VYJ3"
+};
