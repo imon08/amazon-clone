aff5ca30dd048f81120ebcf5204f1b943607f453
Priyosmita <priyosh08@gmail.com>
2022-12-06 01:17:38 +0530

    authentication completed

--- src/Header.js
+++ src/Header.js
@@ -6,14 +6,13 @@ import { useStateValue } from "./StateProvider";
 import { auth } from "./firebase";
 
 function Header() {
-  
   const [{ basket, user }, dispatch] = useStateValue();
 
-  const handleAuthentication=()=>{
-    if(user){
+  const handleAuthentication = () => {
+    if (user) {
       auth.signOut();
     }
-  }
+  };
 
   return (
     <div className="header">
@@ -28,8 +27,8 @@ function Header() {
         <SearchIcon className="header__searchIcon"></SearchIcon>
       </div>
       <div className="header__nav">
-        <Link to="/login">
-          <div Click={handleAuthentication} className="header__option">
+        <Link to={!user && "/login"}>
+          <div onClick={handleAuthentication} className="header__option">
             <span className="header__optionLineOne">Hello Guest</span>
             <span className="header__optionLineTwo">
               {user ? "Sign Out" : "Sign In"}
