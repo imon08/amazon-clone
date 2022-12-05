564a69e11baf9a49102715eded5116898d1fd519
Priyosmita <priyosh08@gmail.com>
2022-12-05 19:57:17 +0530

    login page completed

--- src/App.js
+++ src/App.js
@@ -4,17 +4,33 @@ import "./Header.css";
 import Home from "./Home";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Checkout from "./Checkout";
+import Login from "./Login";
 
 function App() {
   return (
     //BEM
     <Router>
       <div className="app">
-        <Header />
         <Routes>
-          <Route path="/login" />
-          <Route path="/checkout" element={<Checkout />} />
-          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/checkout"
+            element={
+              <div>
+                <Header />
+                <Checkout />
+              </div>
+            }
+          />
+          <Route
+            path="/"
+            element={
+              <div>
+                <Header />
+                <Home />
+              </div>
+            }
+          />
         </Routes>
       </div>
     </Router>
